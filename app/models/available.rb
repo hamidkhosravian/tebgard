f9ca08f8074f4e4e4a50722de965a3503ba5f1d8
hamid <h.khosravian@roundtableapps.com>
2017-12-19 15:41:54 +0330

    add validation uniqueness for activation

--- app/models/available.rb
+++ app/models/available.rb
@@ -1,3 +1,13 @@
 class Available < ApplicationRecord
   belongs_to :availability,  polymorphic: true
+
+  before_create :datetime_uniqueness
+
+  private
+    def datetime_uniqueness
+      availables = availability.availables.where(date: self.date)
+      availables.each do |date|
+        raise BadRequestError, I18n.t("messages.date.date_cover") if (self.start_time..self.end_time).cover?(date.start_time) || (self.start_time..self.end_time).cover?(date.end_time)
+      end if availables.present?
+    end
 end
